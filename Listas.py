250ed3d7c4e3526665c0708eb97d14d1b0a27784
LuizdosReis <luizdosreis@gmail.com>
2017-03-07 16:58:41 -0300

    Exercicio 1 e 2 pagina 13 livro use a cabeça python

---
+++ Listas.py
@@ -0,0 +1,10 @@
+'Exercicio 1 pagina 13'
+movies = ['the holy grail', 'the life of brian', 'the meanin of life']
+movies.insert(1, 1975)
+movies.insert(3, 1979)
+movies.insert(5, 1983)
+print(movies)
+
+'exercicio 2 pagina 13'
+movies1 = ['the holy grail',1975,'the life of brian',1979, 'the meanin of life',1983]
+print(movies1)
