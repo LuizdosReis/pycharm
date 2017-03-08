7c917f3c307ded9ab6bca36545bcd1f7a85de34e
LuizdosReis <luizdosreis@gmail.com>
2017-03-08 16:46:25 -0300

    exercicios capitulo 2

--- nester.py
+++ nester.py
@@ -1,8 +1,15 @@
 """this is the 'nester.py' module and it provides one function called print_lol
 wich prints lists that may or may not include nested lists"""
-def print_lol(the_list):
+
+
+def print_lol(the_list,indent=False,level=0,):
     for item in the_list:
-        if(isinstance(item,list)):
-            print_lol(item)
+        if (isinstance(item, list)):
+            if indent:
+                level = level + 1
+            print_lol(item,indent,level)
         else:
+            for n in range(level):
+                print('\t',end='');
+
             print(item);
