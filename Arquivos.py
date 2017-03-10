9994ff74ec0101bd282eb51a7d344203fa6ed109
LuizdosReis <luizdosreis@gmail.com>
2017-03-10 00:22:39 -0300

    usando o metodo split

--- Arquivos.py
+++ Arquivos.py
@@ -10,5 +10,15 @@ print(os.getcwd())
 data = open('sketch.txt')
 
 for each_line in data:
-    print(each_line,end='')
+    if each_line.find(':') != -1:
+        (role,line_spoken) = each_line.split(':',1)
+        print(role, end='')
+        print(' said:', end='')
+        print(line_spoken, end='')
+    else:
+        print(each_line,end='')
+
+data.close()
+
+
 
