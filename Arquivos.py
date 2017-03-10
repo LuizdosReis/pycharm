e4fc4d991c2902a74e808f9aacdc8c4ff5c8ccb0
LuizdosReis <luizdosreis@gmail.com>
2017-03-10 16:08:18 -0300

    trabalhando com erros mais expecificos

--- Arquivos.py
+++ Arquivos.py
@@ -7,19 +7,19 @@ os.chdir('ArquivosApoio/chapter3')
 #printa novamente a nova pasta
 print(os.getcwd())
 
-data = open('sketch.txt')
+# verifica se o arquivo foi encontrado
+try:
+    data = open('sketch.txt')
 
-for each_line in data:
-    try:
-        (role,line_spoken) = each_line.split(':',1)
-        print(role, end='')
-        print(' said:', end='')
-        print(line_spoken, end='')
-    except:
-        #ignora o erro e continua o programa
-        pass
-
-data.close()
-
-
-
+    for each_line in data:
+        try:
+            (role, line_spoken) = each_line.split(':', 1)
+            print(role, end='')
+            print(' said:', end='')
+            print(line_spoken, end='')
+        except ValueError:
+            # ignora o erro e continua o programa
+            pass
+    data.close()
+except IOError:
+    print('the data file is missing!')
