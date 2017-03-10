824e71ba0eba4aa6d34b413243bff51205f6d1b7
LuizdosReis <luizdosreis@gmail.com>
2017-03-10 17:42:17 -0300

    Alterando o nester para tambem salvar em arquivos usando o nester para salvar nos arquivos de forma mais elegante

--- Arquivos.py
+++ Arquivos.py
@@ -1,4 +1,5 @@
 import os
+import nester
 
 #printa a pasta que voce esta
 print(os.getcwd())
@@ -6,20 +7,33 @@ print(os.getcwd())
 os.chdir('ArquivosApoio/chapter3')
 #printa novamente a nova pasta
 print(os.getcwd())
+man = []
+other = []
 
 # verifica se o arquivo foi encontrado
 try:
     data = open('sketch.txt')
-
     for each_line in data:
         try:
             (role, line_spoken) = each_line.split(':', 1)
-            print(role, end='')
-            print(' said:', end='')
-            print(line_spoken, end='')
+            line_spoken = line_spoken.strip()
+            if (role == 'Man'):
+                man.append(line_spoken)
+
+            elif (role == 'Other Man'):
+                other.append(line_spoken)
+
         except ValueError:
             # ignora o erro e continua o programa
             pass
     data.close()
+except IOError:
+    print('the data file is missing!')
+
+try:
+    with open('OutMan.txt', 'w') as out_man:
+        nester.print_lol(man, file=out_man)
+    with open('OtherMan.txt', 'w') as other_man:
+        nester.print_lol(other, file=other_man)
 except IOError:
     print('the data file is missing!')
--- nester.py
+++ nester.py
@@ -1,15 +1,16 @@
 """this is the 'nester.py' module and it provides one function called print_lol
 wich prints lists that may or may not include nested lists"""
 
+import sys
 
-def print_lol(the_list,indent=False,level=0,):
+
+def print_lol(the_list, indent=False, level=0, file=sys.stdout):
     for item in the_list:
         if (isinstance(item, list)):
             if indent:
                 level = level + 1
-            print_lol(item,indent,level)
+            print_lol(item, indent, level, file)
         else:
             for n in range(level):
-                print('\t',end='');
-
-            print(item);
+                print('\t', end='', file=file);
+            print(item, file=file);
